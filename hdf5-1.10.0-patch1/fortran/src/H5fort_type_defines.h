44b551e16deea5295243727c2be01dc24115607a
tjha <tjha@umich.edu>
2018-03-21 07:25:42 -0400

    Added hdf5 Library

---
+++ hdf5-1.10.0-patch1/fortran/src/H5fort_type_defines.h
@@ -0,0 +1,16 @@
+/* If you are reading this file and it has a '.h' suffix, it was automatically
+ * generated from the '.in' version.  Make changes there.
+ */
+#define H5_FORTRAN_NATIVE_INTEGER_KIND 
+#define H5_FORTRAN_NATIVE_INTEGER_SIZEOF 
+#define H5_FORTRAN_NATIVE_REAL_KIND 
+#define H5_FORTRAN_NATIVE_REAL_SIZEOF 
+#define H5_FORTRAN_NATIVE_DOUBLE_KIND 
+#define H5_FORTRAN_NATIVE_DOUBLE_SIZEOF 
+#define H5_FORTRAN_INTEGER_KINDS 
+#define H5_FORTRAN_INTEGER_KINDS_SIZEOF 
+#define H5_FORTRAN_REAL_KINDS 
+#define H5_FORTRAN_REAL_KINDS_SIZEOF 
+#define H5_HAVE_Fortran_INTEGER_SIZEOF_16 
+#define H5_FORTRAN_HAVE_C_LONG_DOUBLE 
+#define H5_FORTRAN_C_LONG_DOUBLE_IS_UNIQUE 
